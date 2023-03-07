585b913819346fe128fe174789b81615fea2cb1c
Uche Odinamba <121253428+CodeUchay@users.noreply.github.com>
2023-03-07 19:24:55 +0100

    Add files via upload
    This is a Nigerian Trivia application

---
+++ Trivia App/js/questions.js
@@ -0,0 +1,70 @@
+//Create an array and add the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which of the following is the capital city of Nigeria?",
+    answer: "Abuja",
+    options: [
+      "Abuja",
+      "Benin",
+      "Lagos",
+      "Ibadan"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which of the following is the official currency of Nigeria?",
+    answer: "Naira",
+    options: [
+      "Diner",
+      "Euro",
+      "Pounds",
+      "Naira"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which of the following is the official language of Nigeria?",
+    answer: "English",
+    options: [
+      "French",
+      "English",
+      "Spanish",
+      "Dutch"
+    ]
+  },
+    {
+    numb: 4,
+    question: " Nigeria's flag consists of two colors. What are they?",
+    answer: "Green and White",
+    options: [
+      "Green and White",
+      "Green and Red",
+      "White and Blue",
+      "Blue and Green"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What city in Nigeria is Chimamanda Ngozi Adiche From?",
+    answer: "Enugu",
+    options: [
+      "Lagos",
+      "Imo",
+      "Enugu",
+      "Ibadan"
+    ]
+  },
+  //you can add new questions in this format
+  //   {
+  //   numb: 6,
+  //   question: "Your Question",
+  //   answer: "Correct answer",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
